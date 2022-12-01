cfd8a1dfae7669919f2e3273b53880ca803ce832
Jamie Wilson <jamie.wilson@11fs.com>
2022-12-01 14:33:40 +0000

    Day 01 p2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -5,25 +5,37 @@ fn main() -> Result<()> {
     let path = Path::new("./data/day01.txt");
     let lines = BufReader::new(File::open(path)?).lines();
 
-    println!("max_calories = {}", max_calories(lines)?);
+    let totals = count_calories(lines)?;
+
+    println!("max_calories  = {}", max_calories(&totals));
+    println!("top3_calories = {}", top3_calories(&totals));
     Result::Ok(())
 }
 
-fn max_calories<I: Iterator<Item = Result<String>>>(input: I) -> Result<usize> {
-    let mut total_max_reindeer: usize = 0;
+fn count_calories<I: Iterator<Item = Result<String>>>(input: I) -> Result<Vec<usize>> {
+    let mut reindeer_totals: Vec<usize> = Vec::new();
     let mut total_this_reindeer: usize = 0;
     for s in input {
         let line = s?;
         if line.is_empty() {
-            println!("total_this_reindeer = {}", total_this_reindeer);
-            total_max_reindeer = total_max_reindeer.max(total_this_reindeer);
+            reindeer_totals.push(total_this_reindeer);
             total_this_reindeer = 0;
         } else {
             let calories: usize = usize::from_str(&line).map_err(|_| ErrorKind::InvalidData)?;
             total_this_reindeer += calories;
         }
     }
-    Result::Ok(total_max_reindeer.max(total_this_reindeer))
+    reindeer_totals.push(total_this_reindeer);
+    reindeer_totals.sort();
+    Result::Ok(reindeer_totals)
+}
+
+fn max_calories(totals: &Vec<usize>) -> usize {
+    totals[totals.len() - 1]
+}
+
+fn top3_calories(totals: &Vec<usize>) -> usize {
+    totals[totals.len() - 3..].into_iter().sum()
 }
 
 #[cfg(test)]
@@ -31,9 +43,7 @@ mod tests {
 
     use super::*;
 
-    #[test]
-    fn problem1() {
-        const INPUT: &'static str = "1000
+    const INPUT: &'static str = "1000
 2000
 3000
 
@@ -47,9 +57,22 @@ mod tests {
 9000
 
 10000";
+    
+    fn parse_test_data() -> Vec<usize> {
+        count_calories(INPUT.lines().map(|s| Result::Ok(String::from(s)))).unwrap()
+    }
 
-        let max = max_calories(INPUT.lines().map(|s| Result::Ok(String::from(s)))).unwrap();
+    #[test]
+    fn problem1() {
+        let answer = max_calories(&parse_test_data());
 
-        assert_eq!(max, 24000);
+        assert_eq!(answer, 24000);
+    }
+
+    #[test]
+    fn problem2() {
+        let answer = top3_calories(&parse_test_data());
+
+        assert_eq!(answer, 45000);
     }
 }
